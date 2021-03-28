cac70cdad9a19b80371ad604fbfab704dce36232
Ramitsan <ramitsan@gmail.com>
2021-03-28 19:27:49 +0300

    feat: add the solution of the task Transform array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  // throw new CustomError('Not implemented');
   // минимальное число ходов, необходимое для решения головоломки, равно (2 в степени n) − 1, где n — число дисков
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = Math.floor(turns / (turnsSpeed / 3600)); // 1 час = 3600 секунд
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  if (arr.length == 0) return arr;
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+      i++;
+    } else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next' && i > 0) {
+      newArr.pop();
+    } else if (arr[i] === '--double-next' && i !== arr.length - 1) {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next' && i > 0) {
+      newArr.push(arr[i - 1]);
+    } else if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' && arr[i] !== '--discard-next' && arr[i] !== '--discard-prev') {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
